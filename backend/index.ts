4240552fe8d9c1b7c26a8fa71ffca8e7fbf84ee6
badmagick329 <uzairfarooqi@gmail.com>
2024-06-22 18:12:29 +0100

    feat: add basic user assignment and notification button

--- backend/index.ts
+++ backend/index.ts
@@ -1,31 +1,96 @@
-import { serve, ServerWebSocket } from 'bun';
+import { serve } from 'bun';
 
-const clients = new Set<ServerWebSocket>();
 const serverPort = process.env.SERVER_PORT;
+let users: string[] = [];
+const messages: string[] = [];
 
-const server = serve({
+type WebSocketData = {
+  type: string;
+  sender: string;
+  data: string;
+};
+
+const server = serve<WebSocketData>({
   port: serverPort,
   fetch(req, server) {
-    if (server.upgrade(req)) {
+    const newUser = `User_${users.length}`;
+    if (
+      server.upgrade(req, {
+        data: {
+          type: 'assign_user',
+          sender: 'server',
+          data: newUser,
+        },
+      })
+    ) {
       return;
     }
     return new Response('WebSocket server is running');
   },
   websocket: {
     open(ws) {
-      clients.add(ws);
+      const username = ws.data.data;
+      users.push(username);
+      ws.subscribe('chat');
+      ws.subscribe('notification');
+      ws.send(
+        JSON.stringify({
+          type: 'set_user',
+          sender: 'server',
+          data: username,
+        })
+      );
+      server.publish(
+        'chat',
+        JSON.stringify({
+          type: 'message',
+          sender: 'server',
+          data: `${username} joined`,
+        })
+      );
+      // clients.add(ws);
       console.log('Client connected');
-      ws.send('Hi!');
+      console.log(ws.data);
+      console.log('users:', users);
     },
-    message(ws, message) {
-      console.log('Received message:', message);
-      for (const client of clients) {
-        client.send(message);
+    message(ws, data) {
+      console.log('Received message:', data);
+      // @ts-expect-error - ignore
+      const parsed = JSON.parse(data) as WebSocketData;
+
+      if (parsed.type === 'message') {
+        messages.push(`${parsed.sender}: ${parsed.data}`);
+        server.publish(
+          'chat',
+          JSON.stringify({
+            type: 'message',
+            sender: parsed.sender,
+            data: parsed.data,
+          })
+        );
+      } else if (parsed.type === 'notification') {
+        server.publish(
+          'chat',
+          JSON.stringify({
+            type: 'notification',
+            sender: parsed.sender,
+            data: '',
+          })
+        );
       }
     },
     close(ws) {
-      clients.delete(ws);
       console.log('Client disconnected');
+      ws.unsubscribe('chat');
+      users = users.filter((user) => user !== ws.data.data);
+      server.publish(
+        'chat',
+        JSON.stringify({
+          type: 'message',
+          sender: 'server',
+          data: `${ws.data.data} left`,
+        })
+      );
     },
   },
 });
--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,8 +5,15 @@ import { useEffect, useRef, useState } from 'react';
 
 const serverAddress = import.meta.env.VITE_SERVER_ADDRESS;
 
+type WebSocketData = {
+  type: string;
+  sender: string;
+  data: string;
+};
+
 function App() {
   const [messages, setMessages] = useState<string[]>([]);
+  const [user, setUser] = useState<string>('');
   const [inputMessage, setInputMessage] = useState('');
   const socketRef = useRef<WebSocket | null>(null);
 
@@ -14,9 +21,7 @@ function App() {
     socketRef.current = new WebSocket(serverAddress);
 
     socketRef.current.onmessage = (event) => {
-      const sound = new Audio(bellSound);
-      sound.play();
-      setMessages((prevMessages) => [...prevMessages, event.data]);
+      handleMessage(JSON.parse(event.data));
     };
 
     return () => {
@@ -26,8 +31,31 @@ function App() {
 
   const sendMessage = () => {
     if (inputMessage && socketRef.current) {
-      socketRef.current.send(inputMessage);
+      const data = {
+        type: 'message',
+        sender: user,
+        data: inputMessage,
+      };
+      console.log(`Sending`, data);
+      socketRef.current.send(JSON.stringify(data));
       setInputMessage('');
+    }
+  };
+
+  const handleMessage = (data: WebSocketData) => {
+    console.log('handling message', data);
+    if (data.type === 'message') {
+      console.log('updating messages', data.data);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        `${data.sender}: ${data.data}`,
+      ]);
+    } else if (data.type === 'set_user') {
+      console.log('settings user', data.data);
+      setUser(data.data);
+    } else if (data.type === 'notification') {
+      const sound = new Audio(bellSound);
+      sound.play();
     }
   };
 
@@ -39,20 +67,41 @@ function App() {
           <p key={index}>{message}</p>
         ))}
       </div>
-      <div className='flex flex-col items-center max-w-96 gap-4'>
-        <input
-          className='text-black'
-          type='text'
-          value={inputMessage}
-          onChange={(e) => setInputMessage(e.target.value)}
-          onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
-        />
-        <button
-          className='border-2 border-white px-4 py-2'
-          onClick={sendMessage}
-        >
-          Send
-        </button>
+      <div className='flex flex-col items-center max-w-[300px] gap-4'>
+        <div className='flex w-full justify-around gap-4'>
+          <span>{user}</span>
+          <input
+            className='text-black w-full'
+            type='text'
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+            onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
+          />
+        </div>
+        <div className='flex justify-around gap-4'>
+          <button
+            className='border-2 border-white px-2 py-1'
+            onClick={sendMessage}
+          >
+            Send
+          </button>
+          <button
+            className='border-2 border-white px-2 py-1'
+            onClick={() => {
+              if (socketRef.current) {
+                const data = {
+                  type: 'notification',
+                  sender: user,
+                  data: '',
+                };
+                console.log(`Sending`, data);
+                socketRef.current.send(JSON.stringify(data));
+              }
+            }}
+          >
+            Notify
+          </button>
+        </div>
       </div>
     </div>
   );
